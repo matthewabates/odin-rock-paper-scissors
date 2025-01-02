75066b473b4141d793487b6b749b10cef99786cf
matthewabates <matthew.a.bates@gmail.com>
2025-01-02 11:51:37 -0600

    ui

--- script.js
+++ script.js
@@ -1,5 +1,5 @@
 var scores = {
-    human: 0,
+    player: 0,
     computer: 0
 }
 var rules = {
@@ -8,36 +8,57 @@ var rules = {
     scissors: { beats: 'paper' }
 }
 var choices = Object.keys(rules)
+var listener = function(event) {
+    playRound(event.target.id)
+}
+document.querySelectorAll(".selection").forEach((button) => {
+    button.addEventListener("click", listener)
+})
 
 function playGame() {
-    [...Array(5)].forEach(() => {
-        playRound()
-    });
-
-    console.log("Game over")
-    let outcome = `You tied; ${scores.human} to ${scores.computer}`
-    if (scores.human > scores.computer) {
-        outcome = `You won; ${scores.human} to ${scores.computer}`
-    } else if (scores.computer > scores.human) {
-        outcome = `You lose; ${scores.human} to ${scores.computer}`
+    displayResult("Game over")
+    let outcome = `You tied; ${scores.player} to ${scores.computer}`
+    if (scores.player > scores.computer) {
+        outcome = `You won; ${scores.player} to ${scores.computer}`
+    } else if (scores.computer > scores.player) {
+        outcome = `You lose; ${scores.player} to ${scores.computer}`
     }
-    console.log(outcome)
-    alert(outcome)
+    displayResult(outcome)
 }
 
-function playRound() {
+function playRound(playerChoice) {
     let computerChoice = getComputerChoice()
-    let humanChoice = getHumanChoice()
     
-    if (doesBeat(computerChoice, humanChoice)) {
-        console.log(`You lose; ${computerChoice} beats ${humanChoice}`)
+    //TODO display playerchoice and computer choice?
+
+    if (doesBeat(computerChoice, playerChoice)) {
+        displayResult(`You lose; ${computerChoice} beats ${playerChoice}`)
         scores.computer++
-    } else if (doesBeat(humanChoice, computerChoice)) {
-        console.log(`You win; ${humanChoice} beats ${computerChoice}`)
-        scores.human++
+    } else if (doesBeat(playerChoice, computerChoice)) {
+        displayResult(`You win; ${playerChoice} beats ${computerChoice}`)
+        scores.player++
     } else {
-        console.log(`You tied; ${humanChoice} vs ${computerChoice}`)
+        displayResult(`You tied; ${playerChoice} vs ${computerChoice}`)
     }
+
+    document.querySelector("#player").textContent = scores.player
+    document.querySelector("#computer").textContent = scores.computer
+
+    if (isGameOver()) {
+        if (scores.player > scores.computer) {
+            outcome = `You win`
+        } else if (scores.computer > scores.player) {
+            outcome = `You lose`
+        }
+        displayResult(outcome)
+        document.querySelectorAll(".selection").forEach((button) => {
+            button.removeEventListener("click", listener)
+        })
+    }
+}
+
+function isGameOver() {
+    return Math.max(scores.player, scores.computer) >= 5
 }
 
 function doesBeat(first, second) {
@@ -48,14 +69,11 @@ function getComputerChoice() {
     return choices[random(0,2)]
 }
 
-function getHumanChoice() {
-    let choice = prompt("enter your choice") 
-    console.assert("invalid choice", choices.includes(choice))
-    return choice.toLowerCase()  
-}
-
 function random(min, max) {
     return Math.floor((min + Math.random() * (max - min + 1)))
 }
 
-playGame()
+function displayResult(message) {
+    let resultContainer = document.querySelector("#results")
+    resultContainer.textContent = message
+}
